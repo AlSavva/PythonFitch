281e1bb2f1abc170fe34b65c5ca3ba2729857c2e
AlSavva <70343642+alsavva@users.noreply.github.com>
2020-09-01 23:35:42 +0300

    Update insert_search.py
    Add choise_search

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,6 @@
-# Алгоритм бинарного поиска в упорядоченных списках
-
 def binary_search(list, item):
+    """Функция для осуществления метода бинарного поиска
+    в сортированных спискахю Скорость исполнения О(log_n)"""
     low = 0
     high = len(list) - 1
     while low <= high:
@@ -13,3 +13,31 @@ def binary_search(list, item):
         else:
             low = mid + 1
     return None
+
+
+# Алгоритм сортировки выбором. Скорость выполнения О(n**2)
+
+def find_smallest(list):
+    """Функция реализует поиск наименьшего значения в произвольном списке.
+    Возвращает индекс наименьшего элемента в списке."""
+    smallest = list[0]
+    smallest_ind = 0
+    for i in range(1, len(list)):
+        if list[i] < smallest:
+            smallest = list[i]
+            smallest_ind = i
+    return smallest_ind
+
+
+def chois_sort(list):
+    """Функция реализует сортировку выбором произвольного списка, используя
+    функцию find_smallest"""
+    new_list = []
+    for i in range(len(list)):
+        smallest = find_smallest(list)
+        new_list.append(list.pop(smallest))
+    return new_list
+
+
+my_list = list(map(int, input().split()))
+print(chois_sort(my_list))
