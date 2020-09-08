750174ce93d00054190c60c9d097075716d3aa2c
Al_Savva <savva2003@gmail.com>
2020-09-08 12:49:31 +0300

    modefied binary_search.py

--- trening.py
+++ trening.py
@@ -23,10 +23,19 @@ def my_maxlst(list):
     if len(list) == 2:
         return list[0] if list[0] > list[1] else list[1]
     sub_max = my_maxlst(list[1:])
-    return sub_max if sub_max>list[0] else list[0]
+    return sub_max if sub_max > list[0] else list[0]
 
-ml = gen_list(4, 85, 10)
-print(ml)
-print(my_sum(ml))
-print(my_len(ml))
-print(my_maxlst(ml))
+
+def my_numbers(n):
+    if n == 1:
+        return n
+    else:
+        return str(my_numbers(n - 1)) + ' ' + str(n)
+
+
+# ml = gen_list(4, 85, 10)
+# print(ml)
+# print(my_sum(ml))
+# print(my_len(ml))
+# print(my_maxlst(ml))
+print(my_numbers(6))
